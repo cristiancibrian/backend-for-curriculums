2f811c4d0658441bf61701659c93c52f6f97b874
Cristian Cibrian <cristian.cibrian@uabc.edu.mx>
2023-05-10 18:56:35 -0700

    second commit

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+
 
 const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '1234',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_DATABASE || 'curriculums'
 })
 
 module.exports = sequelize;
